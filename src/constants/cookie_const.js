ba23367a135236318e16aa5a4a813e8d2f6fdf71
roramigator <roberto@morado.dev>
2021-01-26 18:45:21 -0500

    barebones template setup with routes and context

---
+++ src/constants/cookie_const.js
@@ -0,0 +1,1 @@
+export const USER_SESSION = "id_8dffac2d-195e";
--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Router } from "react-router-dom";
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+import { Routes } from "./routes/Routes";
+
+import { createBrowserHistory } from "history";
+
+const history = createBrowserHistory();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Router history={history}>
+    <Routes />
+  </Router>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
