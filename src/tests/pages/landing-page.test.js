2f4e57aa5f1cf86647346b02830e423bf9cd1d8d
roramigator <roberto@morado.dev>
2021-01-31 14:16:57 -0500

    testing the content of the components

--- src/tests/pages/landing-page.test.js
+++ src/tests/pages/landing-page.test.js
@@ -1,11 +1,52 @@
 import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
 
-import LandingPage from '../../pages/Landing';
+import { ABOUT, BOARD, STATEMENT } from '../../constants/page_content/landing';
 
-describe('Landing View Test', () => {
-    test('Initial render', () => {
-        const { queryByText } = render(<LandingPage />);
-        expect(queryByText("We're Part of Something Big")).toBeInTheDocument();
+import TextContent from '../../components/TextContent';
+import StatementContent from '../../components/StatementContent';
+import BoardMembers from '../../components/BoardMembers';
+
+describe('Landing Page Test', () => {
+    test('About Section', () => {
+        const { queryByText } = render(<TextContent title={ABOUT.TITLE} text={ABOUT.PARAGRAPHS} />);
+
+        expect(queryByText(ABOUT.TITLE)).toBeInTheDocument();
+
+        ABOUT.PARAGRAPHS.map((paragraph) => {
+            expect(queryByText(paragraph)).toBeInTheDocument();
+        });
+    });
+
+    test('Statemant Section', () => {
+        const { queryByText } = render(
+            <StatementContent text={STATEMENT.PARAGRAPHS} hero={STATEMENT.HERO} />
+        );
+
+        STATEMENT.PARAGRAPHS.map((paragraph) => {
+            expect(queryByText(paragraph)).toBeInTheDocument();
+        });
+
+        expect(queryByText(STATEMENT.HERO)).toBeInTheDocument();
+    });
+
+    test('Board Section', () => {
+        const { queryByText } = render(<TextContent title={BOARD.TITLE} text={BOARD.PARAGRAPHS} />);
+
+        expect(queryByText(BOARD.TITLE)).toBeInTheDocument();
+
+        // TODO: test board paragraphs
+        //
+        // BOARD.PARAGRAPHS.map((paragraph) => {
+        //     expect(queryByText(paragraph)).toBeInTheDocument();
+        // });
+    });
+
+    test('Statemant Section', () => {
+        const { queryByText } = render(<BoardMembers members={BOARD.MEMBERS} />);
+
+        BOARD.MEMBERS.map((member) => {
+            expect(queryByText(member.name)).toBeInTheDocument();
+        });
     });
 });
