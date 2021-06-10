56ed2d21505df0ab0b878961d838e03c386a8ef7
Moses Amiedor <atmdboss@yahoo.com>
2021-06-11 02:42:04 +0300

    drawer remodel

--- src/components/Drawer/Drawer.jsx
+++ src/components/Drawer/Drawer.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 
+import Logo from '../../assets/BFT-logo.png';
 import { ReactComponent as CloseIcon } from '../../assets/icons/close-icon.svg';
 
 const Drawer = ({ open, onClose, children }) => {
     return (
         <div className={`drawer ${open ? 'open' : ''}`.trim()}>
-            <span className="icon">
-                <CloseIcon onClick={onClose} />
-            </span>
-            <div className="content">{children}</div>
+            <div className="drawer__content">
+                <div className="head">
+                    <div className="logo">
+                        <img src={Logo} alt="logo" />
+                    </div>
+                    <div className="icon">
+                        <CloseIcon onClick={onClose} />
+                    </div>
+                </div>
+                <div className="children">{children}</div>
+            </div>
         </div>
     );
 };
